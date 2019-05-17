8ebbe516c0cec04c445ad5dc8ac50ec3d2838c2d
Matthieu <matthieunadeau96@gmail.com>
2019-05-17 12:19:17 -0400

    added styling to the Die component

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import './Die.css';
+
 class Die extends Component {
 
   render() {
     return (
-      <i className={`fas fa-dice-${this.props.face}`}></i>
+      <i className={`Die fas fa-dice-${this.props.face}`}></i>
     );
   }
 
